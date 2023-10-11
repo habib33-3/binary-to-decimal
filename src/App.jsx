0ef27032191751ca91d44956cd4623a1e69f8699
Habibur Rahman <habib.swi16@gmail.com>
2023-10-11 14:28:26 +0600

    project finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [decimal, setDecimal] = useState(null);
+
+  const handleConvert = (e) => {
+    e.preventDefault();
+    const binary = e.target.binary.value.trim();
+
+    if (binary === "") {
+      return alert("Please enter a number");
+    }
+
+    const binaryPattern = /^[01]+$/;
+    if (!binaryPattern.test(binary)) {
+      return alert("Please enter a valid binary number");
+    }
+
+    const decimalValue = parseInt(binary, 2);
+    setDecimal(decimalValue);
+    e.target.reset();
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="my-10 p-6 w-full lg:w-3/5 mx-auto">
+      <h1 className="text-center font-extrabold text-6xl ">
+        Binary To Decimal
+      </h1>
+      <div className="card p-12 w-full mx-auto">
+        <form
+          action=""
+          onSubmit={handleConvert}
+        >
+          <input
+            type="number"
+            name="binary"
+            id=""
+            className="input input-bordered w-11/12 mx-auto"
+          />
+          <button
+            type="submit"
+            className="btn"
+          >
+            Convert
+          </button>
+        </form>
+
+        <div className="my-6 text-3xl font-bol">Decimal: {decimal}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
